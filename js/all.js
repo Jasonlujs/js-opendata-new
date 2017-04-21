014f31a334776c1d41040ebdc1e73e56ee68f464
Jasonlujs <aw26000@gmail.com>
2017-04-21 18:10:12 +0800

    Update all.js

--- js/all.js
+++ js/all.js
@@ -2713,8 +2713,7 @@ var data =
   }
 }
 
-///// data end
-      var api = data.result.records;
+///// data end      var api = data.result.records;
       var str = [];
       var area = document.getElementById('mySelect');
       var list = document.querySelector('.main');
@@ -2729,43 +2728,22 @@ var data =
             value: str[i],
             text: str[i]
           }));
-
-$(function () {
-  $.getJSON('all.json', function (data) {
-    thisData = JSON.parse(data)
-    var api = thisData.result.records;
-    var str = [];
-    var area = document.getElementById('mySelect');
-    var list = document.querySelector('.main');
-
-    function add() {
-      for (var i in api) {
-        if (str.indexOf(api[i].Zone) == -1) {
-          str.push(api[i].Zone);
-
         }
       }
-      for (var i in str) {
-        $('#mySelect').append($('<option>', {
-          value: str[i],
-          text: str[i]
-        }));
-      }
-    }
-    add();
+      add();
 
-    function updateList(e) {
-      var select = e.target.value;
-      var name = '';
-      for (var i = 0; i < api.length; i++) {
-        if (select == api[i].Zone) {
+      function updateList(e) {
+        var select = e.target.value;
+        var name = '';
+        for (var i = 0; i < api.length; i++) {
+          if (select == api[i].Zone) {
 
-          name += '<h2>' + api[i].Zone + '</h2><div class="content"><div style="width: 100%;height: 155px; position: relative;background-image: url(' + api[i].Picture1 + ');background-size: cover; background-position: center center; "><h3>' + api[i].Name + '</h3><h4>' + api[i].Zone + '</h4></div><div class="clock"><img src="image/icons_clock.png">' + api[i].Opentime + '</div><div class="pin"><img src="image/icons_pin.png">' + api[i].Add + '</div><div class="phone"><img src="image/icons_phone.png">' + api[i].Tel + '</div><div class="ticket"><img src="image/icons_tag.png">' + api[i].Ticketinfo + '</div></div>';
+            name += '<h2>' + api[i].Zone + '</h2><div class="content"><div style="width: 100%;height: 155px; position: relative;background-image: url(' + api[i].Picture1 + ');background-size: cover; background-position: center center; "><h3>' + api[i].Name + '</h3><h4>' + api[i].Zone + '</h4></div><div class="clock"><img src="image/icons_clock.png">' + api[i].Opentime + '</div><div class="pin"><img src="image/icons_pin.png">' + api[i].Add + '</div><div class="phone"><img src="image/icons_phone.png">' + api[i].Tel + '</div><div class="ticket"><img src="image/icons_tag.png">' + api[i].Ticketinfo + '</div></div>';
 
+          }
         }
+        list.innerHTML = name;
       }
-      list.innerHTML = name;
-    }
 
 
       $('#gotop').click(function () {
@@ -2774,6 +2752,7 @@ $(function () {
         }, 900);
       });
       area.addEventListener('change', updateList, false)
+
 ////// 由於不能使用http，所以將資料直接擷取為data,以下為ajax的方法
 
 
